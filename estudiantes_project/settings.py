9d853888750dae5a8c2b0c3b7570a535dcedc64b
Baquero5 <cbaquerov@unemi.edu.ec>
2025-11-02 23:32:42 -0500

    Upgrade

--- estudiantes_project/settings.py
+++ estudiantes_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,7 +79,7 @@ DATABASE_URL = os.environ.get('DATABASE_URL')
 
 if DATABASE_URL:
     # Configuración para GitHub Actions o producción
-    import dj_database_url
+
     DATABASES = {
         'default': dj_database_url.config(
             default=DATABASE_URL,
